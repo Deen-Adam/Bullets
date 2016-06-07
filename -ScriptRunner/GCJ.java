274736188025e65586da6b5ed1b03b8e9cff8769
Deen Adam <deensname@gmail.com>
2016-06-08 02:59:48 +0800

    Update GCJ.java

--- -ScriptRunner/GCJ.java
+++ -ScriptRunner/GCJ.java
@@ -28,6 +28,6 @@ public class GCJ {
 //		out = new PrintWriter( new FileOutputStream(file + "_" + System.currentTimeMillis() + ".out") );
 		solve(); out.flush();
 	}
-	static BufferedReader in; static PrintWriter out;
+	//static BufferedReader in; static PrintWriter out;
 	//static BufferedReader in; static PrintWriter out;
 }
