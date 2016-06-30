4bae53bdf474c09f57264941840786cf45f276ea
Deen Adam <deensname@gmail.com>
2016-06-30 14:16:57 +0800

    Update core.java

--- -ScriptRunner/core.java
+++ -ScriptRunner/core.java
@@ -4,10 +4,11 @@ import java.io.*;
 import java.math.*;
 
 public class core {
-
+	String program = "BEST PROGRAM EVER";
 	public static void main (String args[] ) {
 
 		System.out.println("Testing 123456" +14.5);
+		System.out.println("HELLO WORLD" +program);
 
 	}
 }
