f0bce1d41c27dd16dbacbec4765c5623a0934ba3
Deen Adam <deensname@gmail.com>
2016-07-04 14:04:30 +0800

    Update Multigraph.java

--- FlightSchedule/Multigraph.java
+++ FlightSchedule/Multigraph.java
@@ -2,14 +2,13 @@ package FlightScheduler;
 
 // MULTIGRAPH.JAVA
 // Multigraph class
-//
+
 // A Multigraph contains a collection of Vertex objects, each of which
 // maintains its own adjacency list of edges (see the Vertex and Edge 
 // classes).
-//
+
 // To enumerate the vertices in the Multigraph, you can simply ask for
 // the number of vertices n, then call get() successively on 0 .. n-1.
-//
 
 import java.util.*;
 
